05d2d02c3dcdd1eb8ee782353395029b73477375
El Hector <a291528@uach.mx>
2017-03-07 17:48:22 -0700

    cambio de rutas

--- routes/login_form.js
+++ routes/login_form.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Login/Register page. */
 router.get('/', function(req, res, next) {
-  res.render('register/login_form', { title: 'Express' });
+  res.render('login/login_form', { title: 'Express' });
 });
 
 module.exports = router;
