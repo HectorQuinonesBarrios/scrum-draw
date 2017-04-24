1e9df2a00605834abfc1e4911bb5e9603742102f
El Hector <a291528@uach.mx>
2017-04-24 17:32:28 -0600

    Create de usuarios

--- controllers/new_project.js
+++ controllers/new_project.js
@@ -1,9 +1,29 @@
 'use strict'
 const express = require('express');
+const Proyecto = require('../models/proyecto');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
 
 function blank(req, res, next) {
   res.render('projects/blank.pug', { title: 'Express' });
 }
+
+function crear(req, res, next){
+  logger.debug("Crear");
+/*
+  let proyecto = new Proyecto({
+    nombre: req.body.nombre,
+  	fecha_solicitud: req.body.fecha_solicitud,
+  	fecha_arranque: req.body.fecha_arranque,
+  	descripcion: req.body.descripcion,
+  	scrum_master: ObjectId,
+  	product_owner: ObjectId,
+  	equipo_desarrollo: [{
+  		usuario_id: ObjectId, rol: String
+  	}]
+  });*/
+}
+
 
 module.exports = {
   blank
--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,41 @@
 'use strict'
 const express = require('express');
+const Usuario = require('../models/usuario')
+const log4js = require('log4js');
+const logger = log4js.getLogger();
 
 function blank(req, res, next) {
   res.render('users/blank');
 }
+function crear(req, res, next){
+  logger.debug("Crear");
+  let usuario = new UsuarioSchema ({
+    nombre: req.body.nombre,
+  	fecha_nacimiento: req.body.fecha_nacimiento,
+  	curp: req.body.curp,
+  	rfc: req.body.rfc,
+  	domicilio: req.body.domicilio,
+  	habilidades: [{nombre: req.body.habilidad, rank: req.body.rango}]
+  });
+  usuario.save((err,object)=>{
+    let code = '';
+    let message = '';
+    if(err){
+      code = 'danger';
+      message = 'Error al crear el usuario';
+    }else{
+      code = 'success';
+      message = 'Usuario creado correctamente';
+    }
+    res.locals.status = {
+      code:code,
+      message:message
+    };
+    next();
+  });
+}
 
 module.exports = {
-  blank
+  blank,
+  crear
 }
