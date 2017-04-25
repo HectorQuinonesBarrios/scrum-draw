a566132b88fc3c4fdd924ba91d8624fa77f1b68b
Jonas Perusquia <jonaspm_99@outlook.com>
2017-04-25 05:54:09 -0600

    Enable Project creation. Is saved to DB

--- controllers/new_project.js
+++ controllers/new_project.js
@@ -8,23 +8,43 @@ function blank(req, res, next) {
   res.render('projects/blank.pug', { title: 'Express' });
 }
 
-function crear(req, res, next){
-  logger.debug("Crear");
-/*
+function crear(req, res, next) {
+  logger.debug("Crear Proyecto");
+	console.log(req.body);
   let proyecto = new Proyecto({
     nombre: req.body.nombre,
   	fecha_solicitud: req.body.fecha_solicitud,
   	fecha_arranque: req.body.fecha_arranque,
   	descripcion: req.body.descripcion,
-  	scrum_master: ObjectId,
-  	product_owner: ObjectId,
-  	equipo_desarrollo: [{
-  		usuario_id: ObjectId, rol: String
-  	}]
-  });*/
+  	scrum_master: req.body.scrum_master,
+  	product_owner: req.body.product_owner,
+		equipo_desarrollo: req.body.equipo_desarrollo
+//	, equipo_desarrollo: [{
+//  		usuario_id: ObjectId, rol: String
+//  	}]
+  });
+
+	proyecto.save((err, object)=>{
+		let code;
+		let message;
+		if(err){
+		  code = 'danger';
+		  message = 'No se ha podido crear el proyecto.';
+		}else{
+		  code = 'success';
+		  message = 'Proyecto creado correctamente.';
+		}
+
+		res.locals.status = {
+		  code,
+		  message
+		};
+		res.redirect('/');
+	});
 }
 
 
 module.exports = {
-  blank
+  blank,
+  crear
 }
--- routes/new_project.js
+++ routes/new_project.js
@@ -5,4 +5,6 @@ const new_projectController = require('../controllers/new_project');
 /* GET home page. */
 router.get('/', new_projectController.blank);
 
+router.post('/', new_projectController.crear);
+
 module.exports = router;
