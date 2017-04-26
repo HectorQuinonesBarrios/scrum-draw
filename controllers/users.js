cd546899fc43b3922ddca2cebd320256de5b166c
Jonas Perusquia <jonaspm_99@outlook.com>
2017-04-25 18:54:56 -0600

    Display projects from DB.

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 'use strict'
 const express = require('express');
-const Usuario = require('../models/usuario')
+const Usuario = require('../models/usuario');
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 const bcrypt = require('bcrypt-nodejs');
