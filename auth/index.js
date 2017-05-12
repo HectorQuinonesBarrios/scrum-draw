4346997f208555ba37e2db7c29d6a1269c19bcbd
Hector <a291528@uach.mx>
2017-05-12 15:55:07 -0600

    Se agrega la api de github

--- auth/index.js
+++ auth/index.js
@@ -1,53 +1,46 @@
 const express = require('express'),
-  passport = require('passport'),
-  FacebookStrategy = require('passport-facebook').Strategy,
-  TwitterStrategy = require('passport-twitter').Strategy,
-  GoogleStrategy = require('passport-google-oauth').OAuthStrategy,
-  Usuario = require('../models/usuario'),
-  log4js = require('log4js'),
-  logger = log4js.getLogger();
+    passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy,
+    TwitterStrategy = require('passport-twitter').Strategy,
+    GitHubStrategy = require('passport-github2').Strategy,
+    Usuario = require('../models/usuario'),
+    log4js = require('log4js'),
+    logger = log4js.getLogger();
 
 module.exports = () => {
     const FACEBOOK_APP_ID = "1335013806558485",
-      FACEBOOK_APP_SECRET = "07b8b7cc1629df202035471ecdd8e581";
+        FACEBOOK_APP_SECRET = "07b8b7cc1629df202035471ecdd8e581";
     passport.use(new FacebookStrategy({
-        clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_APP_SECRET,
-        callbackURL: "http://localhost:3000/login/auth/facebook/callback"
-      },
-      function(accessToken, refreshToken, profile, cb) {
-        Usuario.create({
-          facebookId: profile.id
-        }, function(err, user) {
-          return cb(err, user);
-        });
-      }
+            clientID: FACEBOOK_APP_ID,
+            clientSecret: FACEBOOK_APP_SECRET,
+            callbackURL: "http://localhost:3000/login/auth/facebook/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            logger.debug(profile);
+        }
     ));
 
     const TWITTER_CONSUMER_KEY = "eaq0EKS6UGo3lJCI9VGRchpFh",
-      TWITTER_CONSUMER_SECRET = "iOfj0WfdrzMSNlXk1xNHoMHZooJEjIXpveL435oXx4g2dFWGbW";
+        TWITTER_CONSUMER_SECRET = "iOfj0WfdrzMSNlXk1xNHoMHZooJEjIXpveL435oXx4g2dFWGbW";
     passport.use(new TwitterStrategy({
-        consumerKey: TWITTER_CONSUMER_KEY,
-        consumerSecret: TWITTER_CONSUMER_SECRET,
-        callbackURL: "http://localhost:3000/login/auth/twitter/callback"
-      },
-      function(token, tokenSecret, profile, done) {
-        logger.debug(profile);
-      }
+            consumerKey: TWITTER_CONSUMER_KEY,
+            consumerSecret: TWITTER_CONSUMER_SECRET,
+            callbackURL: "http://localhost:3000/login/auth/twitter/callback"
+        },
+        function(token, tokenSecret, profile, done) {
+            logger.debug(profile);
+        }
     ));
-    const GOOGLE_CONSUMER_KEY = "",
-          GOOGLE_CONSUMER_SECRET= "";
-    passport.use(new GoogleStrategy({
-        consumerKey: GOOGLE_CONSUMER_KEY,
-        consumerSecret: GOOGLE_CONSUMER_SECRET,
-        callbackURL: "http://localhost:3000/login/auth/google/callback"
-      },
-      function(token, tokenSecret, profile, done) {
-        User.findOrCreate({
-          googleId: profile.id
-        }, function(err, user) {
-          return done(err, user);
-        });
-      }
+
+    const GITHUB_CLIENT_ID = "1af7c256a143b9a09d73",
+        GITHUB_CLIENT_SECRET = "3cc7f2bf1cdeb2b11e143d7f4f93f5d5ab656944";
+    passport.use(new GitHubStrategy({
+            clientID: GITHUB_CLIENT_ID,
+            clientSecret: GITHUB_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/login/auth/github/callback"
+        },
+        function(accessToken, refreshToken, profile, done) {
+            logger.debug(profile);
+        }
     ));
 }
--- routes/login.js
+++ routes/login.js
@@ -5,25 +5,41 @@ const loginController = require('../controllers/login');
 
 /* GET Login/Register page. */
 router.get('/auth/facebook',
-  passport.authenticate('facebook'));
+    passport.authenticate('facebook'));
 
 router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+    passport.authenticate('facebook', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 router.get('/auth/twitter',
-  passport.authenticate('twitter'));
+    passport.authenticate('twitter'));
 
 router.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+    passport.authenticate('twitter', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+router.get('/auth/github',
+    passport.authenticate('github', {
+        scope: ['user:email']
+    }));
 
+router.get('/auth/github/callback',
+    passport.authenticate('github', {
+        failureRedirect: '/login'
+    }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 router.get('/', loginController.login_form);
 router.post('/', loginController.login);
 
