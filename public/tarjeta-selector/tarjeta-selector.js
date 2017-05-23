b9001a2dbabc4da0329f483d7f3b093ec3925a51
Jonas Perusquia <jonaspm_99@outlook.com>
2017-05-23 01:08:58 -0600

    Fix backlogs with Riot

--- public/tarjeta-selector/tarjeta-selector.js
+++ public/tarjeta-selector/tarjeta-selector.js
@@ -1,24 +1,20 @@
 
-riot.tag2('tarjeta-selector', '<div class="row" id="sortableKanbanBoards"> <div class="panel panel-primary kanban-col" each="{backlog, i in backlogs}"> <div class="panel-heading">{backlog.tipo}<i class="fa fa-2x fa-plus-circle pull-right"></i></div> <div class="panel-body"> <div class="kanban-centered" id="{backlog._id}"> <article class="kanban-entry grab" id="item6" draggable="true" each="{tarjeta, j in backlog.tarjetas}"> <div class="kanban-entry-inner"> <div class="kanban-label"> <h2>Tarea con valor de {tarjeta.valor}</h2> <p>Como : {tarjeta.narrativa.como}</p> <p>Quiere : {tarjeta.narrativa.quiero}</p> <p>De Manera Que : {tarjeta.narrativa.manera}</p> <p>Dado : {tarjeta.criterios.dado}</p> <p>Cuando : {tarjeta.criterios.cuando}</p> <p>Entonces : {tarjeta.criterios.entonces}</p> </div> </div> </article> </div> </div> <div class="panel-footer"> <button class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#CardModal" data-backlog="{backlog._id}">Agregar tarjeta</button> </div> </div> </div> <div class="modal modal-static fade" id="processing-modal" role="dialog" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-body"> <div class="text-center"><i class="fa fa-refresh fa-5x fa-spin"></i> <h4>Processing...</h4> </div> </div> </div> </div> </div>', '', '', function(opts) {
-});
-riot.tag2('script', '', '', '', function(opts) {
-  this.backlogs = opts.backlogs
-  this.proyecto = opts.proyecto
-  let self = this
-  this.socket = io('http://localhost:3000', {forceNew: true});
-  let xhttp = new XMLHttpRequest();
-  this.socket.on('backlogs', (backlog)=>{
+riot.tag2('tarjeta-selector', '<div class="row" id="sortableKanbanBoards"> <div class="panel panel-primary kanban-col" each="{backlog, i in backlogs}"> <div class="panel-heading">{backlog.tipo}<i class="fa fa-2x fa-plus-circle pull-right"></i></div> <div class="panel-body"> <div class="kanban-centered" id="{backlog._id}"> <article class="kanban-entry grab" id="item6" draggable="true" each="{tarjeta, i in backlog.tarjetas}"> <div class="kanban-entry-inner"> <div class="kanban-label"> <h2>Tarea con valor de {tarjeta.valor}</h2> <p>Como : {tarjeta.narrativa.como}</p> <p>Quiere : {tarjeta.narrativa.quiero}</p> <p>De Manera Que : {tarjeta.narrativa.manera}</p> <p>Dado : {tarjeta.criterios.dado}</p> <p>Cuando : {tarjeta.criterios.cuando}</p> <p>Entonces : {tarjeta.criterios.entonces}</p> </div> </div> </article> </div> </div> <div class="panel-footer"> <button class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#CardModal" data-backlog="{backlog._id}">Agregar tarjeta</button> </div> </div> </div> <div class="modal modal-static fade" id="processing-modal" role="dialog" aria-hidden="true"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-body"> <div class="text-center"><i class="fa fa-refresh fa-5x fa-spin"></i> <h4>Processing...</h4> </div> </div> </div> </div> </div>', '', '', function(opts) {
+    this.backlogs = opts.backlogs
+    this.proyecto = opts.proyecto
+    let self = this
+    this.socket = io('http://localhost:3000', {forceNew: true})
+    let xhttp = new XMLHttpRequest()
+    this.socket.on('backlogs', (backlog)=>{
+      console.log('conectado');
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(xhttp.responseText);
+           self.update({backlogs: JSON.stringify(xhttp.responseText)})
 
-    console.log('conectado');
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-
-         console.log(xhttp.responseText);
-         self.update({backlogs:JSON.parse(xhttp.responseText)});
-
-      }
-    };
-    xhttp.open("GET", "/backlogs/" + this.proyecto._id, true);
-    xhttp.send();
-  });
+        }
+      };
+      xhttp.open('GET', '/backlogs/'+this.proyecto._id, true);
+      xhttp.send();
+    });
 });
--- views/tarjeta-selector/tarjeta-selector.tag
+++ views/tarjeta-selector/tarjeta-selector.tag
@@ -1,48 +1,44 @@
 tarjeta-selector
-  #sortableKanbanBoards.row
-      div.panel.panel-primary.kanban-col(each='{backlog, i in backlogs}')
-        div.panel-heading {backlog.tipo}
+  div#sortableKanbanBoards.row
+      div.panel.panel-primary.kanban-col(each='{ backlog, i in backlogs }')
+        div.panel-heading { backlog.tipo }
           i.fa.fa-2x.fa-plus-circle.pull-right
         div.panel-body
-          div.kanban-centered(id='{backlog._id}')
-              article#item6.kanban-entry.grab(draggable='true' each='{tarjeta, j in backlog.tarjetas}')
+          div.kanban-centered(id='{ backlog._id }')
+              article#item6.kanban-entry.grab(draggable='true' each='{ tarjeta, i in backlog.tarjetas }')
                 div.kanban-entry-inner
                   div.kanban-label
-                    h2 Tarea con valor de {tarjeta.valor}
-                    p Como : {tarjeta.narrativa.como}
-                    p Quiere : {tarjeta.narrativa.quiero}
-                    p De Manera Que : {tarjeta.narrativa.manera}
-                    p Dado : {tarjeta.criterios.dado}
-                    p Cuando : {tarjeta.criterios.cuando}
-                    p Entonces : {tarjeta.criterios.entonces}
+                    h2 Tarea con valor de { tarjeta.valor }
+                    p Como : { tarjeta.narrativa.como }
+                    p Quiere : { tarjeta.narrativa.quiero }
+                    p De Manera Que : { tarjeta.narrativa.manera }
+                    p Dado : { tarjeta.criterios.dado}
+                    p Cuando : { tarjeta.criterios.cuando }
+                    p Entonces : { tarjeta.criterios.entonces }
         div.panel-footer
-          button.btn.btn-info.btn-sm(type='button' data-toggle='modal' data-target='#CardModal' data-backlog='{backlog._id}') Agregar tarjeta
-  #processing-modal.modal.modal-static.fade(role='dialog' aria-hidden='true')
+          button.btn.btn-info.btn-sm(type='button' data-toggle='modal' data-target='#CardModal' data-backlog='{ backlog._id }') Agregar tarjeta
+  div#processing-modal.modal.modal-static.fade(role='dialog' aria-hidden='true')
     div.modal-dialog
       div.modal-content
         div.modal-body
           div.text-center
             i.fa.fa-refresh.fa-5x.fa-spin
             h4 Processing...
-script.
-  this.backlogs = opts.backlogs
-  this.proyecto = opts.proyecto
-  let self = this
-  this.socket = io('http://localhost:3000', {forceNew: true});
-  let xhttp = new XMLHttpRequest();
-  this.socket.on('backlogs', (backlog)=>{
-    
-    console.log('conectado');
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
+  script.
+    this.backlogs = opts.backlogs
+    this.proyecto = opts.proyecto
+    let self = this
+    this.socket = io('http://localhost:3000', {forceNew: true})
+    let xhttp = new XMLHttpRequest()
+    this.socket.on('backlogs', (backlog)=>{
+      console.log('conectado');
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(xhttp.responseText);
+           self.update({backlogs: JSON.stringify(xhttp.responseText)})
 
-         console.log(xhttp.responseText); 
-         self.update({backlogs:JSON.parse(xhttp.responseText)});
-            
-      }
-    };
-    xhttp.open("GET", "/backlogs/" + this.proyecto._id, true);
-    xhttp.send();
-  });
-
-  
+        }
+      };
+      xhttp.open('GET', '/backlogs/'+this.proyecto._id, true);
+      xhttp.send();
+    });
